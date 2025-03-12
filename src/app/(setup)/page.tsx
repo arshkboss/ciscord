3b4e929d40b9ef2e6626bc965eab2732a20f6275
Arsh <arshtechiekhan@gmail.com>
2025-03-13 02:09:02 +0530

    boiler plate setup user check initiate

---
+++ src/app/(setup)/page.tsx
@@ -0,0 +1,38 @@
+import { initialProfile } from "@/utils/initial-profile";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import prisma from "@/utils/prisma";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log(user);
+  const profile = await initialProfile();
+  const server = await prisma.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile?.id,
+        },
+      },
+    },
+  });
+
+  console.log(server);
+  if (server) {
+    return redirect(`servers/${server.id}`);
+  } else {
+    console.log(`no server found`, server);
+  }
+
+  return (
+    <div>
+      Create a Server
+      <LoginLink>Login</LoginLink>
+      <LogoutLink>Logout</LogoutLink>
+    </div>
+  );
+};
+
+export default SetupPage;
---
+++ src/utils/initial-profile.ts
@@ -0,0 +1,30 @@
+import { redirect } from "next/navigation";
+import prisma from "./prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export const initialProfile = async () => {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+  console.log(user);
+  if (!user) {
+    return redirect("/test");
+  }
+
+  const profile = await prisma.profile.findUnique({
+    where: { userId: user.id },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await prisma.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.given_name} ${user.family_name}`,
+      imgUrl: user.picture || "",
+      email: user.email || "",
+    },
+  });
+};
