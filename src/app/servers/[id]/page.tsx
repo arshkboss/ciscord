efa81be35e22e8c1843264b19ca3e650328ab8d9
Arsh <arshtechiekhan@gmail.com>
2025-03-13 04:54:41 +0530

    made a basic server page

---
+++ src/app/servers/[id]/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import prisma from "@/utils/prisma";
+import Image from "next/image";
+const ServerPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  console.log(id);
+
+  const getServerDetails = await prisma.server.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  console.log(typeof getServerDetails);
+  return (
+    <div>
+      {
+        <div className="flex flex-col">
+          <p>{`Server Created at: ${getServerDetails?.createdAt.toDateString()}`}</p>
+          <p>
+            {
+              <Image
+                src={getServerDetails?.imageUrl || ""}
+                alt={getServerDetails?.imageUrl || ""}
+                height={100}
+                width={100}
+                unoptimized
+              ></Image>
+            }
+          </p>
+          <p>{`Server Name: ${getServerDetails?.name.toUpperCase()}`}</p>
+          <p>{`Invite Code: ${getServerDetails?.inviteCode}`}</p>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default ServerPage;
